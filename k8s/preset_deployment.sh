f53a1df0c4005d9b19e6193343b107cb66180635
ihudak <ihudak@gmail.com>
2023-08-21 15:24:01 +0200

    Add the possibility to reduce the number of external IP addresses on public clouds

--- k8s/preset_deployment.sh
+++ k8s/preset_deployment.sh
@@ -4,9 +4,10 @@ display_usage() {
   echo "Bad/no argument(s) supplied";
       echo "Sets agent instrumentation and architecture in the deployment yaml files"
       echo "Usage:";
-      echo "   ./preset_deployment.sh -agents  -arm|-x64  # takes docker image with OA and OTel agents";
-      echo "   ./preset_deployment.sh -noagent -arm|-x64  # takes docker image with no agents embedded";
+      echo "   ./preset_deployment.sh -agents  -arm|-x64 {-minimizePorts} # takes docker image with OA and OTel agents";
+      echo "   ./preset_deployment.sh -noagent -arm|-x64 {-minimizePorts} # takes docker image with no agents embedded";
       echo "   ./preset_deployment.sh -reset              # resets yamls";
+      echo "                   -minimizePorts makes most k8s services ClusterIP to reduce allocating public IP address"
       echo;
       echo "Please supply at least either -agents, -noagent or -reset";
       echo "      optionally specify platform as -arm or -x64";
@@ -25,6 +26,9 @@ reset_settings() {
   sed -i.bak "s/-arm64:latest/-{ARCH}:latest/g" databases.yaml
   sed -i.bak "s/bookstore-webapp-x64:latest/bookstore-webapp-{ARCH}:latest/g" bookstore.yaml
   sed -i.bak "s/bookstore-webapp-arm64:latest/bookstore-webapp-{ARCH}:latest/g" bookstore.yaml
+  # setting LoadBalancer back
+  sed -i.bak "s/type: ClusterIP # LoadBalancer/type: LoadBalancer # ClusterIP/g" *.yaml
+  sed -i.bak -E "/^#.+nodePort: / s/^#//g" *.yaml
   rm *.bak
 }
 
@@ -46,5 +50,12 @@ reset_settings
 sed -i.bak "s/-{AGENT}-{ARCH}:latest/-$AGENT-$PLATFORM:latest/g" *.yaml
 sed -i.bak "s/-{ARCH}:latest/-$PLATFORM:latest/g" databases.yaml
 sed -i.bak "s/bookstore-webapp-{ARCH}:latest/bookstore-webapp-$PLATFORM:latest/g" bookstore.yaml
+
+# set ClusterIP and comment nodePorts
+if [ $3 = "-minimizePorts" ] || [ $2 = "-minimizePorts" ] || [ $1 = "-minimizePorts" ]; then
+    sed -i.bak "s/type: LoadBalancer # ClusterIP/type: ClusterIP # LoadBalancer/g" *.yaml
+    sed -i.bak -E "/^      nodePort: / s/^#*/#/g" *.yaml
+fi
+
 rm *.bak
 echo "Preparation done: $AGENT $PLATFORM"
