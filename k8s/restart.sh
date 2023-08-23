be259f430f3054460cb6c52f2961db43eaf822f1
ihudak <ihudak@gmail.com>
2023-08-23 11:09:36 +0200

    Update the readme (preset-deployment)

--- k8s/restart.sh
+++ k8s/restart.sh
@@ -19,13 +19,13 @@ kubectl apply -f ingest.yaml
 kubectl apply -f ratings.yaml
 kubectl apply -f bookstore.yaml
 
-k delete deployment clients
-k delete deployment books
-k delete deployment carts
-k delete deployment storage
-k delete deployment orders
-k delete deployment payments
-k delete deployment dynapay
-k delete deployment superpay
-k delete deployment ratings
-k delete deployment ingest
+#k delete deployment clients
+#k delete deployment books
+#k delete deployment carts
+#k delete deployment storage
+#k delete deployment orders
+#k delete deployment payments
+#k delete deployment dynapay
+#k delete deployment superpay
+#k delete deployment ratings
+#k delete deployment ingest
