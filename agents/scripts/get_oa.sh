65b33752ed304ed9a1559650cf91df048fdd07e5
ihudak <ihudak@gmail.com>
2023-09-24 22:18:45 +0200

    separate agent configuration scripts

--- agents/scripts/get_oa.sh
+++ agents/scripts/get_oa.sh
@@ -3,21 +3,26 @@ mkdir -p /opt/app && mkdir -p  /var/lib/dynatrace/oneagent && \
 # install unzip (OA will be downloaded as a zip-archive)
 apt-get update \
   && apt-get install -y unzip \
-  && rm -rf /var/lib/apt/lists/* &&
+  && rm -rf /var/lib/apt/lists/*
 
 # Set URL and creds to download OA
-export DT_AGENT_DOWNLOAD_TENANT_URL=`cat /opt/env | grep DT_AGENT_DOWNLOAD_TENANT_URL | sed s/DT_AGENT_DOWNLOAD_TENANT_URL=//`
-export DT_AGENT_DOWNLOAD_TOKEN=`cat /opt/env | grep DT_AGENT_DOWNLOAD_TOKEN | sed s/DT_AGENT_DOWNLOAD_TOKEN=//`
+## Workaround for the tailing slash in the URL
+export TENANT_URL=`echo $TENANT_URL | sed -E 's/[ '\$'/'']+\$//'`
+
+export PLATFORM=`uname -p`
+if [ $PLATFORM = "arm64" ] || [ $PLATFORM = "aarch64" ]; then
+  export PLATFORM="arm";
+elif [ $PLATFORM = "x86_64" ]; then
+  export PLATFORM="x86";
+fi
+
 
 # Download OA (Java Agent only)
 curl --request GET -sL \
---url "$DT_AGENT_DOWNLOAD_TENANT_URL/api/v1/deployment/installer/agent/unix/paas/latest?flavor=default&arch=x86&bitness=64&include=java&skipMetadata=true" \
+--url "$TENANT_URL/api/v1/deployment/installer/agent/unix/paas/latest?flavor=default&arch=$PLATFORM&bitness=64&include=java&skipMetadata=true" \
 --header 'accept: */*' \
---header "Authorization: Api-Token $DT_AGENT_DOWNLOAD_TOKEN" \
+--header "Authorization: Api-Token $OA_TOKEN" \
 --output '/var/lib/dynatrace/oneagent/OneAgent.zip'
-
-# Remove file with the confidential data
-rm /opt/env
 
 # Unzip OA
 cd /var/lib/dynatrace/oneagent && unzip /var/lib/dynatrace/oneagent/OneAgent.zip && rm /var/lib/dynatrace/oneagent/OneAgent.zip
@@ -25,20 +30,13 @@ cd /var/lib/dynatrace/oneagent && unzip /var/lib/dynatrace/oneagent/OneAgent.zip
 # Configure OA
 # create a template of the config file
 export OA_CONF_FILE=/var/lib/dynatrace/oneagent/agent/conf/standalone.conf
-echo "tenant tenant_id"                             >> $OA_CONF_FILE
-echo "tenantToken tenant_token"                     >> $OA_CONF_FILE
-echo "serverAddress {tenant_url:443/communication}" >> $OA_CONF_FILE
 
 # Get OA-ingest token
-curl -X GET \$TENANT_URL/api/v1/deployment/installer/agent/connectioninfo \
+export TENANT_TOKEN=$(curl -X GET \$TENANT_URL/api/v1/deployment/installer/agent/connectioninfo \
   -H \"accept: application/json\" \
   -H \"Authorization: Api-Token \$OA_TOKEN\" | \
 grep tenantToken | \
-sed s/\ \ \"tenantToken\"\ :\ \"// | sed s/\",// > /opt/tenant.token
-# token is in tenant.token file. Get it into an Env Var
-export TENANT_TOKEN=$(cat /opt/tenant.token)
-# remove the file with token
-rm /opt/tenant.token
+sed s/\ \ \"tenantToken\"\ :\ \"// | sed s/\",//)
 
 # Set OA data ingest endpoint and creds in the OA config file
 sed -i s/tenant_id/\$TENANT_ID/g $OA_CONF_FILE
--- agents/scripts/get_otel.sh
+++ agents/scripts/get_otel.sh
@@ -2,9 +2,13 @@
 export OTEL_RESOURCE_ATTRIBUTES="service.name=java-quickstart,service.version=1.0.1"
 export OTEL_METRICS_EXPORTER=none
 export OTEL_EXPORTER_OTLP_TRACES_PROTOCOL=http/protobuf
+
+## Workaround for the tailing slash in the URL
+export TENANT_URL=`echo $TENANT_URL | sed -E 's/[ '\$'/'']+\$//'`
 ## Set Otel env variables that come from the monitoring tenant
 export OTEL_EXPORTER_OTLP_TRACES_ENDPOINT=$TENANT_URL/api/v2/otlp/v1/traces
 export OTEL_EXPORTER_OTLP_TRACES_HEADERS=Authorization="Api-Token $OTEL_TOKEN"
-wget -O /opt/opentelemetry-javaagent.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/$OTEL_VER/opentelemetry-javaagent.jar
+wget -O /opt/opentelemetry-javaagent.jar https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/latest/download/opentelemetry-javaagent.jar
+
 # Do not forget to make this file executable
 ###chmod +x /opt/otel.sh
