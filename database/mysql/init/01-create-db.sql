215c32c0c6f902be41c80bc1255aa3b8f6b9b1dc
Ivan Gudak <ivan.gudak@dynatrace.com>
2023-10-31 22:06:21 +0100

    Fix MySQL Server startup

--- database/mysql/init/01-create-db.sql
+++ database/mysql/init/01-create-db.sql
@@ -3,11 +3,3 @@ create database dt_books_carts;
 create database dt_books_payments;
 create database dt_books_dynapays;
 create database dt_books_ratings;
-
-grant all privileges on dt_books_clients.* to user@'%';
-grant all privileges on dt_books_carts.* to user@'%';
-grant all privileges on dt_books_payments.* to user@'%';
-grant all privileges on dt_books_dynapays.* to user@'%';
-grant all privileges on dt_books_ratings.* to user@'%';
-
-flush privileges;
