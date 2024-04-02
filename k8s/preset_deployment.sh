311da8bc3e896e5c417e530d95f88964766d5a4d
Ivan Gudak <ihudak@gmail.com>
2024-04-02 22:42:47 +0200

    Build dockers on github

--- k8s/preset_deployment.sh
+++ k8s/preset_deployment.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 display_usage() {
-  echo "Sets agent instrumentation and architecture in the deployment yaml files"
+  echo "Sets agent instrumentation in the deployment yaml files"
   echo "Usage:";
-  echo "   ${0} -gyes -ax64 -pmin -n bookstore  # installs agents on deploy; x64 architecture; minimizes external port usage; bookstore namespace";
-  echo "   ${0} -gpre -aarm -pall -n bookstore  # takes agents prebuilt in the docker image; arm (apple) architecture; every service will be open (callable from outside of the k8s)";
+  echo "   ${0} -gyes -pmin -n bookstore  # installs agents on deploy; minimizes external port usage; bookstore namespace";
+  echo "   ${0} -gpre -pall -n bookstore  # takes agents prebuilt in the docker image; every service will be open (callable from outside of the k8s)";
   echo "   ${0} -r                   # resets yamls";
   echo "Flags:";
   echo " -g - agent: yes/no/pre. default = yes - preloads otel and dynatrace java agents. pre - use preinstrumented images";
-  echo " -a - architecture: arm/x64. default = x64 - sets architecture";
   echo " -p - ports for k8s services: all/min. default = min - defines whether all k8s should get an external IP. Min - only ingest";
   echo " -n - namespace. default bookstore";
   echo;
@@ -20,16 +19,10 @@ display_usage() {
 reset_settings() {
   echo Resetting the config...
   # reset settings
-  sed -i.bak "s/-noagent-x64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-agents-x64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-preinstrument-x64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-noagent-arm64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-agents-arm64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-preinstrument-arm64:latest/-{AGENT}-{ARCH}:latest/g" *.yaml
-  sed -i.bak "s/-x64:latest/-{ARCH}:latest/g" databases.yaml
-  sed -i.bak "s/-arm64:latest/-{ARCH}:latest/g" databases.yaml
-#  sed -i.bak "s/bookstore-webapp-x64:latest/bookstore-webapp-{ARCH}:latest/g" bookstore.yaml
-#  sed -i.bak "s/bookstore-webapp-arm64:latest/bookstore-webapp-{ARCH}:latest/g" bookstore.yaml
+  sed -i.bak "s/-noagent:latest/-{AGENT}:latest/g" *.yaml
+  sed -i.bak "s/-agents:latest/-{AGENT}:latest/g" *.yaml
+  sed -i.bak "s/-preinstrument:latest/-{AGENT}:latest/g" *.yaml
+#  sed -i.bak "s/bookstore-webapp:latest/bookstore-webapp:latest/g" bookstore.yaml
   # setting LoadBalancer back
   sed -i.bak "s/type: ClusterIP # LoadBalancer/type: LoadBalancer # ClusterIP/g" *.yaml
   sed -i.bak -E "/^#.+nodePort: / s/^#//g" *.yaml
@@ -41,19 +34,15 @@ reset_settings() {
 
 get_params() {
   ag="yes"       # default - with agents
-  ar="x64"       # default - x64
   po="min"       # default - all k8s have their public IPs
   ns="bookstore" # default namespace
   rs="no"        # no reset by default
   hl="no"        # help
-  xx="boo"       # placeholder for x flag (can be used to force x64 arch)
   while getopts hg:a:x:p:n:r flag
   do
       case "${flag}" in
         h) hl="yes";;
         g) ag=${OPTARG};;
-        a) ar=${OPTARG};;
-        x) xx=${OPTARG};;
         p) po=${OPTARG};;
         n) ns=${OPTARG};;
         r) rs="yes";;
@@ -63,9 +52,6 @@ get_params() {
 
   # fixing most common typos
   if [ $ag != "yes" ] && [ $ag != "y" ] && [ $ag != "pre" ] && [ $ag != "p" ]; then ag="no";    fi
-  if [ $ar = "arm" ] || [ $ar = "rm64" ] || [ $ar = "rm" ];                    then ar="arm64"; fi # cover -aarm64, -arm64, -aarm, -arm
-  if [ $ar != "arm64" ];                                                       then ar="x64";   fi
-  if [ $xx = "86" ] || [ $xx = "64" ];                                         then ar="x64";   fi
   if [ $po != "min" ];                                                         then po="all";   fi
   if [ $ns = "" ];                                                             then ns="bookstore"; fi
 
@@ -77,7 +63,6 @@ get_params() {
   else # doing the job. show configuration first
     echo Parameters configured:
     echo " agents:    " $ag;
-    echo " arch:      " $ar;
     echo " ports:     " $po;
     echo " namespace: " $ns;
   fi
@@ -95,9 +80,8 @@ elif [ $ag = "pre" ] || [ $ag = "p" ]; then
 else
   ag="agents";
 fi
-sed -i.bak "s/-{AGENT}-{ARCH}:latest/-$ag-$ar:latest/g" *.yaml
-sed -i.bak "s/-{ARCH}:latest/-$ar:latest/g" databases.yaml
-#sed -i.bak "s/bookstore-webapp-{ARCH}:latest/bookstore-webapp-$ar:latest/g" bookstore.yaml
+sed -i.bak "s/-{AGENT}:latest/-$ag:latest/g" *.yaml
+#sed -i.bak "s/bookstore-webapp:latest/bookstore-webapp:latest/g" bookstore.yaml
 
 # set namespace
 sed -i.bak -E "/name:.*$/ s/name:.*$/name: ${ns}/g" namespace.yaml
@@ -110,4 +94,4 @@ if [ $po = "min" ]; then
 fi
 
 rm *.bak
-echo "Preparation done: $ag $ar"
+echo "Preparation done: $ag"
